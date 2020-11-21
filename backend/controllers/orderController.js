4aa9e4670187eb6f7e9c38d3717ef2aff186629d
htet-aung-khant <info@hakhant.online>
2020-11-21 19:17:41 +0630

    Added Order Details Screen

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -3,7 +3,7 @@ import Order from '../models/orderModel.js'
 
 
 // @desc Create new order
-// @route Post /api/products
+// @route Post /api/orders
 // @access Private
 const addOrderItems = asyncHandler(async(req, res, next)=> {
   const { orderItems, shippingAddress, paymentMethod, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body
@@ -13,14 +13,27 @@ const addOrderItems = asyncHandler(async(req, res, next)=> {
     throw new Error('No order items')
   }else {
     const order = new Order({
-       orderItems,user: req.user._id, shippingAddress, paymentMethod, itemsPrice, taxPrice, shippingPrice, totalPrice
+       orderItems, user: req.user._id, shippingAddress, paymentMethod, itemsPrice, taxPrice, shippingPrice, totalPrice
     })
 
     const createdOrder = await order.save()
 
-    res.status(201).json('Created Order...')
+    res.status(201).json(createdOrder)
   }
-
 })
 
-export { addOrderItems }
+// @desc Get order by id
+// @route GET /api/products/:id
+// @access Private
+const getOrderById = asyncHandler(async(req, res)=> {
+  const order = await Order.findById(req.params.id).populate('user', 'name email')
+
+  if(order){
+    res.json(order)
+  }else{
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+export { addOrderItems, getOrderById }
